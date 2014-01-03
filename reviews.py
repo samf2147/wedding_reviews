dca318075d3baa2ece51a7ea38fde924b8fdc22b
Sam Frank <sfrank2147@gmail.com>
2014-01-03 15:23:57 -0600

    added methods for updating attributes

--- reviews.py
+++ reviews.py
@@ -4,11 +4,61 @@ class Venue:
         if isinstance(name, basestring):
             self.name = kwargs['name']
         else:
-            raise TypeError(message = 'Did not pass string for venue name')
+            raise TypeError('Did not pass string for venue name')
+        
+        self.review_list = []
 
 class Review:
     def __init__(self, **kwargs):
-        self.venue = kwargs['venue']
-        self.cost = kwargs['cost']
-        self.num_guests = kwargs['num_guests']
+        #venue must be a subclass of Venue
+        venue = kwargs['venue']
+        cost = kwargs['cost']
+        num_guests = kwargs['num_guests']
+        
+        self.update_cost(cost)
+        self.update_num_guests(num_guests)
+        
+        if isinstance(venue, Venue):
+            self.venue = venue
+        else:
+            raise TypeError('Did not pass Venue subclass as venue parameter')
+        
+        #when review is created, add it to its venue's review list
+        venue.review_list.append(self)
+    
+    def update_venue(self, venue):
+        '''
+        Update the venue of the review object
+        Throws TypeError if it's not passed a Venue object
+        '''
+        #check to make sure you're updating to a Venue object
+        if not isinstance(venue, Venue):
+            raise TypeError('Did not pass Venue subclass as venue parameter')
+        else:
+            self.venue = venue
+            
+        
+    
+    def update_cost(self, cost):
+        '''
+        Update the cost attribute
+        Throw ValueError if cost is less than or equal to 0
+        '''
+        if cost > 0:
+            self.cost = cost
+        else:
+            raise ValueError('cost must be greater than 0')
+    
+    def update_num_guests(self, num_guests):
+        '''
+        Update the number of guests
+        Throw ValueError if number of guests is less than or equal to 0
+        '''
+        num_guests = int(num_guests)
+        if num_guests > 0:
+            self.num_guests = num_guests
+        else:
+            raise ValueError('num_guests must be greater than 0')
+
+
         
--- reviews_tests.py
+++ reviews_tests.py
@@ -7,6 +7,8 @@ class ReviewTests(unittest.TestCase):
         self.venue_two = Venue(name=u'Greentree')
         self.review_one = Review(venue = self.venue_one, cost = 10000.00, num_guests = 200)
         self.review_two = Review(venue = self.venue_one, cost = 20000.00, num_guests = 500)
+        self.review_three = Review(venue = self.venue_two, cost = 20000.00, num_guests=30.5)
+        
     
     def test_venue_constructors(self):
         self.assertEqual(self.venue_one.name, 'Surf Club')
@@ -17,8 +19,37 @@ class ReviewTests(unittest.TestCase):
         self.assertTrue(self.review_one.venue is self.venue_one) 
         self.assertEqual(self.review_one.cost, 10000.00)
         self.assertEqual(self.review_one.num_guests, 200)
+        
     
     def test_out_of_bounds_assignments(self):
         #can't enter string for venue name
         self.assertRaises(TypeError, Venue, name=5)
+        
+        #venue must be a venue object
+        self.assertRaises(TypeError, Review, venue = 'Surf Club', cost = 10000.00, num_guests = 200)
+        
+        #cost and num_guests must both be greater than 0
+        self.assertRaises(ValueError, Review, venue = self.venue_one, cost = 0, num_guests = 200)
+        self.assertRaises(ValueError, Review, venue = self.venue_one, cost = 10000.00, num_guests = 0)
+        
+        #after construction, num_guests must be an int
+        self.assertTrue(isinstance(self.review_three.num_guests,int))
+    
+    def test_review_in_venue_list(self):
+        #check that creating the revue adds it to the venue's list
+        self.assertTrue(self.review_one in self.venue_one.review_list)
+        self.assertTrue(self.review_three not in self.venue_one.review_list)
+    
+    def test_update_review(self):
+        self.review_two.update_cost(15000.00)
+        self.assertEqual(self.review_two.cost, 15000.00)
+        
+        self.review_two.update_num_guests(160)
+        self.assertEqual(self.review_two.num_guests,160)
+        
+        #review_three is the only review for venue 2
+        update_venue_result = self.review_three.update_venue(self.venue_one)
+        self.assertTrue(self.review_three.venue is self.venue_one)
+
+        
         
